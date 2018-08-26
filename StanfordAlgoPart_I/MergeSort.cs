b5b6ee7d57bcaa6950e08942d0f87881924e06b3
aashutoshsingh <aashutoshsingh@gmail.com>
2018-08-26 06:58:05 +0530

    - Added merge tests - Added boundary condition handling

---
+++ StanfordAlgoPart_I/MergeSort.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StanfordAlgoPart_I
+{
+    public class MergeSort
+    {
+        public static int [] Merge(int [] firstHalf, int [] secondHalf)
+        {
+            if (firstHalf == null && secondHalf == null)
+                return null;
+            if (firstHalf == null)
+                return secondHalf;
+            if (secondHalf == null)
+                return firstHalf;
+
+            int[] outputArray = new int[firstHalf.Length + secondHalf.Length];
+            if (outputArray.Length == 0)
+                return outputArray;
+
+            int firstHalfPointer = 0;
+            int secondHalfPointer = 0;
+
+            for (int i = 0; i < outputArray.Length; i++)
+            {
+                if (firstHalfPointer == firstHalf.Length)
+                {
+                    outputArray[i] = secondHalf[secondHalfPointer];
+                    secondHalfPointer++;
+                    continue;
+                }
+
+                if (secondHalfPointer == secondHalf.Length)
+                {
+                    outputArray[i] = firstHalf[firstHalfPointer];
+                    firstHalfPointer++;
+                    continue;
+                }
+
+                if (firstHalf[firstHalfPointer] <= secondHalf[secondHalfPointer])
+                {
+                    outputArray[i] = firstHalf[firstHalfPointer];
+                    firstHalfPointer++;
+                }
+                else
+                {
+                    outputArray[i] = secondHalf[secondHalfPointer];
+                    secondHalfPointer++;
+                }
+            }
+            return outputArray;
+        }
+    }
+}
---
+++ StanfordAlgoPart_ITests/MergeSortTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StanfordAlgoPart_I;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StanfordAlgoPart_I.Tests
+{
+    [TestClass()]
+    public class MergeSortTests
+    {
+        [TestMethod()]
+        public void MergeTest()
+        {
+            int[] firstHalf = { 1, 2, 3, 4 };
+            int[] secondHalf = { 1, 2, 3, 4 };
+            AssetMerge(firstHalf, secondHalf, new int[] { 1, 1, 2, 2, 3, 3, 4, 4 });
+        }
+
+        [TestMethod()]
+        public void MergeTest_2()
+        {
+            int[] firstHalf = { 1, 2};
+            int[] secondHalf = { 1, 2, 3, 4 };
+            AssetMerge(firstHalf, secondHalf, new int[] { 1, 1, 2, 2, 3, 4 });
+        }
+
+        [TestMethod()]
+        public void MergeTest_3()
+        {
+            int[] firstHalf = { 5 };
+            int[] secondHalf = { 1, 2, 3, 4 };
+            AssetMerge(firstHalf, secondHalf, new int[] { 1, 2, 3, 4, 5 });
+        }
+
+        private void AssetMerge(int[] firstHalf, int[] secondHalf, int[] expectedMergeArray)
+        {
+            var outputArray = MergeSort.Merge(firstHalf, secondHalf);
+            Assert.IsTrue(outputArray.SequenceEqual(expectedMergeArray));
+        }
+    }
+}
