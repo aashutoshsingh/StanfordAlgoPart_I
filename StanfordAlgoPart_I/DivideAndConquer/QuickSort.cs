bf3104edc7e4a016c12b32c39d67f49135233227
aashutoshsingh <aashutoshsingh@gmail.com>
2018-09-03 11:16:04 +0530

    - Quick sort partition imlpementation - restructuring of files

---
+++ StanfordAlgoPart_I/DivideAndConquer/QuickSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StanfordAlgoPart_I.DivideAndConquer
+{
+    public class QuickSort
+    {
+
+        /// <summary>
+        /// this function will place the start index element of array at 
+        /// correct position of array i.e. all the elements to left should be smaller and all the elements
+        /// to the right should be greater than it
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="endIndex"></param>
+        /// <returns></returns>
+        public int Partition(int [] array, int startIndex, int endIndex)
+        {
+            if (endIndex == startIndex)
+                return startIndex;
+
+            int partitionIndex = startIndex+1;
+
+            for (int unseenStartIndex = startIndex+1; unseenStartIndex <= endIndex; unseenStartIndex++)
+            {
+                if(array[unseenStartIndex] < array[startIndex])
+                {
+                    SwapArrayElement(array, partitionIndex, unseenStartIndex);
+                    partitionIndex++;
+                }
+            }
+
+            SwapArrayElement(array, startIndex, partitionIndex-1);
+            return partitionIndex - 1;
+        }
+
+        private static void SwapArrayElement(int[] array, int i, int j)
+        {
+            int temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+        }
+    }
+}
---
+++ StanfordAlgoPart_ITests/DivideAndConquer/QuickSortTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StanfordAlgoPart_I.DivideAndConquer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StanfordAlgoPart_I.DivideAndConquer.Tests
+{
+    [TestClass()]
+    public class QuickSortTests
+    {
+        [TestMethod()]
+        public void PartitionTest()
+        {
+            var array = new int[] { 3, 8, 2, 5, 1, 4, 7, 6 };
+            var expectedOutputPos = 2;
+            AssertQuickSortPartition(array, expectedOutputPos);
+        }
+
+        [TestMethod()]
+        public void PartitionTest_1()
+        {
+            var array = new int[] { 7, 8, 2, 5, 1, 4, 3, 6 };
+            var expectedOutputPos = 6;
+            AssertQuickSortPartition(array, expectedOutputPos);
+        }
+
+        [TestMethod()]
+        public void PartitionTest_2()
+        {
+            var array = new int[] { 1, 8, 2, 5, 7, 4, 3, 6 };
+            var expectedOutputPos = 0;
+            AssertQuickSortPartition(array, expectedOutputPos);
+        }
+
+        private static void AssertQuickSortPartition(int[] array, int expectedOutputPos)
+        {
+            QuickSort quickSort = new QuickSort();
+            int pos = quickSort.Partition(array, 0, array.Length - 1);
+            Assert.AreEqual(expectedOutputPos, pos);
+        }
+    }
+}
