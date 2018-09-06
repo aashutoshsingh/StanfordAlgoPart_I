b4f715691e5836a01b5d392beda541165118f9e6
aashutoshsingh <aashutoshsingh@gmail.com>
2018-09-06 10:20:19 +0530

    Added randomize selection and its unit tests

---
+++ StanfordAlgoPart_I/DivideAndConquer/RandomizeSelection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StanfordAlgoPart_I.DivideAndConquer
+{
+    public class RandomizeSelection
+    {
+        public int GetNthSmallestElement(int[] array, int index)
+        {
+            if (index < 1 || index > array.Length)
+                throw new ArgumentOutOfRangeException("Index cannot be less than 0 or more than or equal to size of array");
+
+            if (array.Length == 1)
+                return array[0];
+
+            QuickSort quickSort = new QuickSort();
+            int pos = quickSort.Partition(array, 0, array.Length-1);
+            pos++;//make it 1 based from zero based
+
+            if (pos == index)
+                return array[pos-1];//while returning we should return element based on 0 based indexing
+            else if (pos > index)
+                return GetNthSmallestElement(array.Take(pos).ToArray(), index);
+            else if (pos < index)
+                return GetNthSmallestElement(array.Skip(pos).ToArray(), index - pos);
+
+            return -1;
+        }
+    }
+}
---
+++ StanfordAlgoPart_ITests/DivideAndConquer/RandomizeSelectionTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StanfordAlgoPart_I.DivideAndConquer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StanfordAlgoPart_I.DivideAndConquer.Tests
+{
+    [TestClass()]
+    public class RandomizeSelectionTests
+    {
+        [TestMethod()]
+        public void GetNthSmallestElementTest()
+        {
+            int[] arr = { 2, 4, 32, 5, 32, 8, 65 };
+            int nthSmallest = 3;
+            int expectedElement = 5;            
+            AssertRandomizeSelection(arr, nthSmallest, expectedElement);
+        }
+
+        [TestMethod()]
+        public void GetNthSmallestElementTest_1()
+        {
+            int[] arr = { 2, 4, 32, 5, 32, 8, 65 };
+            int nthSmallest = 6;
+            int expectedElement = 32;
+            AssertRandomizeSelection(arr, nthSmallest, expectedElement);
+        }
+
+        [TestMethod()]
+        public void GetNthSmallestElementTest_2()
+        {
+            int[] arr = { 2, 4, 32, 5, 32, 8, 65 };
+            int nthSmallest = 5;
+            int expectedElement = 32;
+            AssertRandomizeSelection(arr, nthSmallest, expectedElement);
+        }
+
+        [TestMethod()]
+        public void GetNthSmallestElementTest_3()
+        {
+            int[] arr = { 2, 4, 32, 5, 32, 8, 65 };
+            int nthSmallest = 8;
+            int expectedElement = 32;
+            AssertRandomizeSelection(arr, nthSmallest, expectedElement);
+        }        
+
+        private void AssertRandomizeSelection(int [] arr, int nthSmallest, int expectedElement)
+        {
+            var randomizeSelection = new RandomizeSelection();
+            var outputElement = randomizeSelection.GetNthSmallestElement(arr, nthSmallest);
+            Assert.AreEqual(expectedElement, outputElement);
+        }
+    }
+}
